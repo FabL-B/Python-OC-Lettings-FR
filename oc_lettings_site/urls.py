c3d1ccf34758ccc2146d85a73952a4cf6f0a1e20
FabL-B <errebelneibaf@gmail.com>
2025-03-12 18:19:49 +0100

    Add custom 404 and 500 error pages handling.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
+from oc_lettings_site.views import custom_404, custom_500, index
 
-from . import views
+handler404 = "oc_lettings_site.views.custom_404"
+handler500 = "oc_lettings_site.views.custom_500"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('lettings/', include('lettings.urls', namespace="lettings")),
     path('profiles/', include('profiles.urls', namespace="profiles")),
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def custom_404(request, exception):
+    """Display 404 error"""
+    return render(request, "404.html", status=404)
+
+def custom_500(request):
+    """Display 500 error"""
+    return render(request, "500.html", status=500)
