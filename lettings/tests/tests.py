aa5e10c976be483030065b23bc6fdf6e350cc7ab
FabL-B <errebelneibaf@gmail.com>
2025-04-11 08:27:37 +0200

    test avant soutenance

--- lettings/tests/tests.py
+++ lettings/tests/tests.py
@@ -4,36 +4,3 @@ from pytest_django.asserts import assertTemplateUsed, assertContains
 from lettings.models import Address, Letting
 
 
-@pytest.mark.django_db
-def test_index_view(client):
-    """Test lettings index page."""
-    response = client.get(reverse("lettings:index"))
-
-    assert response.status_code == 200
-    assertTemplateUsed(response, "lettings/index.html")
-    assert response.status_code == 200
-
-
-@pytest.mark.django_db
-def test_letting_detail_view(client):
-    """Test letting detail page."""
-    address = Address.objects.create(
-        number=10, street="Main Street", city="New York",
-        state="NY", zip_code=10001, country_iso_code="USA"
-    )
-    letting = Letting.objects.create(title="Nice Apartment", address=address)
-
-    response = client.get(
-        reverse("lettings:letting_detail", args=[letting.id])
-    )
-
-    assert response.status_code == 200
-    assertTemplateUsed(response, "lettings/letting.html")
-    assertContains(response, "Nice Apartment")
-
-
-@pytest.mark.django_db
-def test_letting_detail_view_invalid_id(client):
-    """Test letting detail page for a non-existent letting returns 404."""
-    response = client.get(reverse("lettings:letting_detail", args=[999]))
-    assert response.status_code == 404
